f05f3a4b1a3e8d8adf3c5d627375726d4a926e92
Thomas <tg.59290@gmail.com>
2017-11-04 00:12:36 +0100

    admin views fucntionnal for users articles and categories

--- src/CoreBundle/Controller/AdminController.php
+++ src/CoreBundle/Controller/AdminController.php
@@ -10,10 +10,23 @@ class AdminController extends Controller
 {
 
   /**
-   * @Route("/admin", name ="admin")
+   * @Route("/admin/index", name ="adminIndex")
    */
   public function indexAction()
   {
     return $this->render('CoreBundle:Admin:adminIndex.html.twig');
+  }
+
+  /**
+   * @Route("/admin/view/{entity}", requirements={"category" : "[a-z]+"}, name ="adminView")
+   */
+  public function viewAction($entity)
+  {
+    $em  = $this->getDoctrine()->getManager();
+    $entityRepository = $em->getRepository('CoreBundle:' . $entity);
+
+    $entities = $entityRepository->findAll();
+
+    return $this->render('CoreBundle:Admin/Views:' . $entity . 'View.html.twig', ["entities"=>$entities]);
   }
 }
