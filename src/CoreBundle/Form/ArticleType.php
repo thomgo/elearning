07807bf04c0024e3a959803818aef4cb532a3e5e
Thomas <tg.59290@gmail.com>
2017-12-09 14:45:06 +0100

    made a basic view for index article in the admin

---
+++ src/CoreBundle/Form/ArticleType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')->add('content')->add('date')->add('image')->add('categories');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CoreBundle\Entity\Article'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'corebundle_article';
+    }
+
+
+}
