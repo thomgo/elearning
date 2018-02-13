09e887022e5007b9ecafcf72dbad0753a9829f12
Thomas <tg.59290@gmail.com>
2018-02-13 17:11:01 +0100

    added function to get the new order of elements after drag and drop in main.js

--- web/JS/main.js
+++ web/JS/main.js
@@ -1,6 +1,16 @@
-$(".ajaxRequest").click(function(){
-  alert($(this).html());
-});
-
-$( "#sortable" ).sortable();
+$( "#sortable" ).sortable({
+      update: function( event, ui ) {
+        Dropped();
+      }
+    });
 $( "#sortable" ).disableSelection();
+
+function Dropped() {
+  var trElements = document.getElementsByClassName("sortableItem");
+  var elementsPosition = [];
+  for (var i = 0; i < trElements.length; i++) {
+    var positionAndTrTitle = [i, trElements[i].children[1].innerHTML];
+    elementsPosition.push(positionAndTrTitle);
+  }
+  alert(elementsPosition);
+}
