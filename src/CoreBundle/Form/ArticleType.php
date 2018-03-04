5f0c97f21c2ef66a920036c64b2b18de8dc7946e
Thomas <tg.59290@gmail.com>
2018-03-04 18:58:41 +0100

    higher textarea on admin article in main.css

--- src/CoreBundle/Form/ArticleType.php
+++ src/CoreBundle/Form/ArticleType.php
@@ -6,6 +6,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 
 class ArticleType extends AbstractType
 {
@@ -16,7 +18,7 @@ class ArticleType extends AbstractType
     {
         $builder
         ->add('title')
-        ->add('content')
+        ->add('content', TextareaType::class, array('attr' => array('class' => 'articleContent')))
         ->add('date')
         //The user is not forced to submit an image therefor this form is not required
         ->add('image', ImageType::class, [
