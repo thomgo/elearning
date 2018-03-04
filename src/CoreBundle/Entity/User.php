fffe5a43f25d315f20b5944b61c9127b5ffc410c
Thomas <tg.59290@gmail.com>
2018-03-04 23:21:15 +0100

    added bio and age attribut to user class and override the default registration form

--- src/CoreBundle/Entity/User.php
+++ src/CoreBundle/Entity/User.php
@@ -19,9 +19,71 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="bio", type="text", nullable = true)
+     */
+    private $bio;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="age", type="integer")
+     */
+    private $age;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Set bio
+     *
+     * @param string $bio
+     *
+     * @return Article
+     */
+    public function setBio($bio)
+    {
+        $this->bio = $bio;
+
+        return $this;
+    }
+
+    /**
+     * Get bio
+     *
+     * @return string
+     */
+    public function getBio()
+    {
+        return $this->bio;
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     *
+     * @return Module
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return int
+     */
+    public function getAge()
+    {
+        return $this->age;
     }
 }
