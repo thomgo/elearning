1c7105fe4f3100d8ef3437bba895b9f6e49e25c7
Thomas <tg.59290@gmail.com>
2018-04-09 16:00:54 +0200

    made basic CRUD with doctrine fot test entity

---
+++ src/CoreBundle/Form/TestType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TestType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CoreBundle\Entity\Test'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'corebundle_test';
+    }
+
+
+}
